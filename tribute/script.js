6b8491224c06fafaf31272570364deaf74303242
Tuhin Thakur <111550237+Tuhin810@users.noreply.github.com>
2023-01-25 13:32:11 +0530

    Add files via upload

---
+++ tribute/script.js
@@ -0,0 +1,15 @@
+// function changeStyle() {
+//     var element = document.getElementById("myDiv");
+//     element.style.backgroundColor = "#fff";
+//     element.style.transitionDuration = "1s";
+
+//     var elemento = document.getElementById("text");
+//     elemento.style.color = "#000000";
+
+    
+// }
+
+function changeStyle() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
